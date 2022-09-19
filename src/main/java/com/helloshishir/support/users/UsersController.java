f6451855a88d36f5dcdedbe79465d32e7571488e
Iftekhar Hossain <ifte.hsn@gmail.com>
2022-09-20 00:51:30 +0600

    users listing

--- src/main/java/com/helloshishir/support/users/UsersController.java
+++ src/main/java/com/helloshishir/support/users/UsersController.java
@@ -1,10 +1,27 @@
 package com.helloshishir.support.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("users")
 public class UsersController {
+
+    @Autowired
+    UserService userService;
+
     // 1. get all users
+    @GetMapping("list")
+    public String getUsersList(ModelMap modelMap) {
+        List<User> userList = userService.findAll();
+        modelMap.put("userList", userList);
+        return "users/list";
+    }
     // 2. add new user
     // 3. update existing user
     // 4. delete existing user
